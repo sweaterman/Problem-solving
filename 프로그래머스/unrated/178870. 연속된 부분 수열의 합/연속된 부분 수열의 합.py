8fcb79a0ade9e615a56db10b9d75a1187d44bbf5
sweaterman <97517492+sweaterman@users.noreply.github.com>
2023-10-04 15:37:05 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -6,7 +6,9 @@ def solution(sequence, k):
     total = 0
     
     while True:
+        # 만약, total 값이 k보다 작거나 같을 때
         if total <= k:
+            # 같을 때, answer의 값과 비교해서 넣어주거나, 안넣거나
             if total == k:
                 if answer:
                     if answer[1] - answer[0] > end - start:
@@ -14,13 +16,18 @@ def solution(sequence, k):
                 else:
                     answer.append(start)
                     answer.append(end)
+            # 작거나 같을 때, end를 뒤로 한칸 밀어주고 다시 계산
             end += 1
+            # 인덱스가 초과하면 반복문 종료
             if end >= len(sequence):
                 break
             total += sequence[end]
+        # 만약, total 값이 k보다 클 때
         else:
+            # start 값을 뒤로 한칸 밀어주고 계산
             total -= sequence[start]
             start += 1
+            # start 값이 end 값보다 넘어가면 반복문 종료
             if start > end:
                 break
     
